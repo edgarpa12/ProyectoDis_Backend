4fcb048f55cf3103c8d4270c8ced49570b1aa9f1
Kendall Tames <tamesfern@gmail.com>
2021-01-16 16:24:18 -0600

    Upload logo fix

--- src/routes/helpers.ts
+++ src/routes/helpers.ts
@@ -40,17 +40,19 @@ export async function saveImages(req: Request, res: Response): Promise<String[]>
   });
 }
 
+const uploadLogo = multer({ storage: storage, fileFilter: imageFilter }).single('logo');
+
 export async function registerImage(req: Request, res: Response): Promise<String> {
   return await new Promise<String>((resolve, reject) => {
-    upload(req, res, function (err: any) {
-        // if ((<any>req).fileValidationError)
-        //   reject((<any>req).fileValidationError);
-        // else if (!req.file)
-        //   reject('Please select an image to upload');
-        // else if (err)
-        //   reject(err);
+    uploadLogo(req, res, function (err: any) {
+        if ((<any>req).fileValidationError)
+          reject((<any>req).fileValidationError);
+        else if (!req.file)
+          reject('Please select an image to upload');
+        else if (err)
+          reject(err);
         // @ts-ignore
-        console.log("Files: ", req.body);
+        console.log("Files: ", req.file);
         resolve("123");
       }
     );
